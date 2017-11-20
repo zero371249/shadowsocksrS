accc218e4e3aeb48c70beb814a008e41ce3846f1
AkaneAkaza <AkaneAkaza@protonmail.com>
2017-11-20 23:29:59 +0800

    fix PyMySQL source

--- setup_cymysql.sh
+++ setup_cymysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf CyMySQL
 rm -rf cymysql
-git clone https://github.com/nakagami/CyMySQL.git
+git clone https://github.com/PyMySQL/CyMySQL.git
 mv CyMySQL/cymysql ./
 rm -rf CyMySQL
