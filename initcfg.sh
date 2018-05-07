4a437330c782c5a31b2faba4a348676527211469
xiaoweigod <32919541+miseryCN@users.noreply.github.com>
2018-05-07 14:12:27 +0800

    回退cymysql版本

--- initcfg.sh
+++ initcfg.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+echo 配置中....
 chmod +x *.sh
 wget https://github.com/nakagami/CyMySQL/archive/REL_0_9_4.tar.gz
-tar -zxvf REL_0_9_4.tar.gz
+tar -zxvf REL_0_9_4.tar.gz >>/dev/zero
 mv CyMySQL-REL_0_9_4 CyMySQL
 mv CyMySQL/cymysql ./
 rm -rf CyMySQL
@@ -9,4 +10,5 @@ chmod +x shadowsocks/*.sh
 cp -n apiconfig.py userapiconfig.py
 cp -n config.json user-config.json
 cp -n mysql.json usermysql.json
+echo 完成，请更改usermysql中的配置信息。
 
