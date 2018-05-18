2e24579ad8f6f5aaff6a93115b7c0c3883f63f78
xiaoweigod <32919541+miseryCN@users.noreply.github.com>
2018-05-18 22:53:14 +0800

    fix bugs
    修复了mv错误

--- initcfg.sh
+++ initcfg.sh
@@ -3,6 +3,8 @@ chmod +x *.sh
 wget https://github.com/nakagami/CyMySQL/archive/REL_0_9_4.tar.gz
 tar -zxvf REL_0_9_4.tar.gz >>/dev/zero
 mv CyMySQL-REL_0_9_4 CyMySQL
+rm -rf cymysql
+mkdir cymysql
 mv CyMySQL/cymysql ./
 rm -rf CyMySQL
 rm -rf REL_0_9_4.tar.gz
